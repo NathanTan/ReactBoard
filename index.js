0b329859ca5e34983c517669dda40b4265833be3
NathanTan <tann@oregonstate.edu>
2017-07-13 19:14:10 -0700

    Update index.js
    Working to implement Redux.

--- index.js
+++ index.js
@@ -1,6 +1,22 @@
 import React from 'react';
 import { render } from 'react-dom';
 import Board from './Board';
+import { Provider } from 'react-redux';
+import { createStore, combineReducers } from 'redux';
+import Position from './Position';
 
+// window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
 
-render(<Board />, document.getElementById('root'));
+const store = createStore(
+  Position,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
+);
+//render(<Board />, document.getElementById('root'));
+
+//@ts-check
+render(
+  <Provider store={store}>
+    <Board />
+  </Provider>,
+  document.getElementById('root'),
+);
