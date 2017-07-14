d182b2419218fdba33946c34516c13da527f206c
NathanTan <tann@oregonstate.edu>
2017-07-13 19:15:12 -0700

    Update Board.js
    Working to implement Redux.

--- Board.js
+++ Board.js
@@ -1,29 +1,44 @@
 import React from 'react';
 import Rook from './Rook';
 import Row from './Row';
-
-const style = {
-    boarder: "2px solid #404040",
-  }
-
+import { connect } from 'react-redux';
 
 class Board extends React.Component {
-  
+  state = {
+    name: 'x',
+  };
+
+  style = {
+    boarder: '2px solid #404040',
+  };
 
   render() {
     return (
       <div style={style}>
-        <Row startSquare="white"/>
-        <Row startSquare="black"/>
-        <Row startSquare="white"/>
-        <Row startSquare="black"/>
-        <Row startSquare="white"/>
-        <Row startSquare="black"/>
-        <Row startSquare="white"/>
-        <Row startSquare="black"/>       
+        <Row startSquare="white" />
+        <Row startSquare="black" />
+        <Row startSquare="white" />
+        <Row startSquare="black" />
+        <Row startSquare="white" />
+        <Row startSquare="black" />
+        <Row startSquare="white" />
+        <Row startSquare="black" />
       </div>
     );
   }
 }
 
-export default Board;
+function mapStateToProps(state) {
+  return {
+    dealers: this.state,
+  };
+}
+// Props to map from the state to the component
+mapStateToProps = state => {
+  return {
+    dealers: this.state,
+  };
+};
+
+//export default Board;
+export default connect(mapStateToProps)(Board);
