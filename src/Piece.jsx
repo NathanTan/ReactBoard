c4743dd01f21adff5fcaa7b361b346581c9a3cff
Tan, Nathan <Nathan.Tan@cdk.com>
2017-08-06 08:59:32 -0700

    Piece now centered in square.

--- src/Piece.jsx
+++ src/Piece.jsx
@@ -4,45 +4,42 @@ import React from 'react';
 //No State, no class
 let Piece = (props) => {
     const style = {
-        // witdh: 50,
-        // height: 50,
-        // display: 'inline-block',
+        'width': 49,
+        'height': 49,
         'zIndex': 5,
-        'paddingLeft': '2px',
     };
 
     let content = null;
 
     const piece = () => {
-         console.log("Piece gets called");
         switch(props.pieceType) {
-                case 'wP': content = <img src="https://upload.wikimedia.org/wikipedia/commons/4/45/Chess_plt45.svg" alt="Pawn" draggable="true" />
+                case 'wP': content = <img src="https://upload.wikimedia.org/wikipedia/commons/4/45/Chess_plt45.svg" alt="Pawn" draggable="true" style={style}/>
                 break;
-                case 'wN': content = <img src="https://upload.wikimedia.org/wikipedia/commons/7/70/Chess_nlt45.svg" alt="Knight" draggable="true" />
+                case 'wN': content = <img src="https://upload.wikimedia.org/wikipedia/commons/7/70/Chess_nlt45.svg" alt="Knight" draggable="true" style={style} />
                 break;
-                case 'wB': content = <img src="https://upload.wikimedia.org/wikipedia/commons/b/b1/Chess_blt45.svg" alt="Bishop" draggable="true" />
+                case 'wB': content = <img src="https://upload.wikimedia.org/wikipedia/commons/b/b1/Chess_blt45.svg" alt="Bishop" draggable="true" style={style} />
                 break;
-                case 'wR': content = <img src="https://upload.wikimedia.org/wikipedia/commons/7/72/Chess_rlt45.svg" alt="Rook" draggable="true" />
+                case 'wR': content = <img src="https://upload.wikimedia.org/wikipedia/commons/7/72/Chess_rlt45.svg" alt="Rook" draggable="true" style={style} />
                 break;
-                case 'wQ': content = <img src="https://upload.wikimedia.org/wikipedia/commons/1/15/Chess_qlt45.svg" alt="Queen" draggable="true" />
+                case 'wQ': content = <img src="https://upload.wikimedia.org/wikipedia/commons/1/15/Chess_qlt45.svg" alt="Queen" draggable="true" style={style} />
                 break;
-                case 'wK': content = <img src="https://upload.wikimedia.org/wikipedia/commons/4/42/Chess_klt45.svg" alt="King" draggable="true" />
+                case 'wK': content = <img src="https://upload.wikimedia.org/wikipedia/commons/4/42/Chess_klt45.svg" alt="King" draggable="true" style={style} />
                 break;
-                case 'bP': content = <img src="https://upload.wikimedia.org/wikipedia/commons/c/c7/Chess_pdt45.svg" alt="Pawn" draggable="true" />
+                case 'bP': content = <img src="https://upload.wikimedia.org/wikipedia/commons/c/c7/Chess_pdt45.svg" alt="Pawn" draggable="true" style={style} />
                 break;
-                case 'bN': content = <img src="https://upload.wikimedia.org/wikipedia/commons/e/ef/Chess_ndt45.svg" alt="Knight" draggable="true" />
+                case 'bN': content = <img src="https://upload.wikimedia.org/wikipedia/commons/e/ef/Chess_ndt45.svg" alt="Knight" draggable="true" style={style} />
                 break;
-                case 'bB': content = <img src="https://upload.wikimedia.org/wikipedia/commons/9/98/Chess_bdt45.svg" alt="Bishop" draggable="true" />
+                case 'bB': content = <img src="https://upload.wikimedia.org/wikipedia/commons/9/98/Chess_bdt45.svg" alt="Bishop" draggable="true" style={style} />
                 break;
-                case 'bR': content = <img src="https://upload.wikimedia.org/wikipedia/commons/f/ff/Chess_rdt45.svg" alt="Rook" draggable="true" />
+                case 'bR': content = <img src="https://upload.wikimedia.org/wikipedia/commons/f/ff/Chess_rdt45.svg" alt="Rook" draggable="true" style={style} />
                 break;
-                case 'bQ': content = <img src="https://upload.wikimedia.org/wikipedia/commons/4/47/Chess_qdt45.svg" alt="Queen" draggable="true" />
+                case 'bQ': content = <img src="https://upload.wikimedia.org/wikipedia/commons/4/47/Chess_qdt45.svg" alt="Queen" draggable="true" style={style} />
                 break;
-                case 'bK': content = <img src="https://upload.wikimedia.org/wikipedia/commons/f/f0/Chess_kdt45.svg" alt="King" draggable="true" />
+                case 'bK': content = <img src="https://upload.wikimedia.org/wikipedia/commons/f/f0/Chess_kdt45.svg" alt="King" draggable="true" style={style} />
                 break;
 
                 default:
-                    content = <div></div>;
+                    content = <div className="emptySquare"></div>;
             }
     }
 
