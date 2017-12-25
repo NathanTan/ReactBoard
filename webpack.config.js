9a4d91af32286f61399cce46e5acd05068335915
NathanTan <tann@oregonstate.edu>
2017-12-24 22:33:17 -0800

    Webpack for client and server now.

--- webpack.config.js
+++ webpack.config.js
@@ -1,14 +1,31 @@
-module.exports = {
-    entry: './src/index.js',
-    module: {
-        loaders: [
-            { test: /\.js$/, loader: 'babel-loader', exclude: /node_modules/ },
-            { test: /\.jsx$/, loader: 'babel-loader', exclude: /node_modules/ }
-
-        ]
+module.exports = [
+    {
+        context: `${__dirname}/src`,
+        entry: './App.js',
+        module: {
+            loaders: [
+                { test: /\.js$/, loader: 'babel-loader', exclude: /node_modules/ },
+                { test: /\.jsx$/, loader: 'babel-loader', exclude: /node_modules/ }
+            ]
+        },
+        output: {
+            filename: 'bundle.js',
+            path: __dirname + '/public'
+        }
     },
-    output: {
-        filename: 'bundle.js',
-        path: __dirname + '/public'
+    {
+        entry: './src/server.js',
+        module: {
+            loaders: [
+                { test: /\.js$/, loader: 'babel-loader', exclude: /node_modules/ },
+                { test: /\.jsx$/, loader: 'babel-loader', exclude: /node_modules/ }
+
+            ]
+        },
+        target: 'node',
+        output: {
+            filename: 'server.js',
+            path: __dirname + '/public'
+        }
     }
-}
+]
